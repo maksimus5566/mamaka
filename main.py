28fa9389d0462b719f04e3dea367040c3879f158
maksimus5566 <78495435+maksimus5566@users.noreply.github.com>
2021-02-03 20:48:58 +0200

    Lesson 2

---
+++ main.py
@@ -0,0 +1,21 @@
+a = "hello"
+print(type(a))
+#str>string - строка, всегда в ковычках
+
+b=1234
+print(b)
+print(type(b))
+#int>integer - целове число (также всегда в ковычках)
+
+c=12.35
+print(type(c))
+#float – бробные числа, число с плавующей запятой
+
+boolean_value = True
+print(type(boolean_value))
+#boolean – да/нет и тд (булеан)
+
+#none_type -
+
+split()
+#разделитель при перечисленние данных
